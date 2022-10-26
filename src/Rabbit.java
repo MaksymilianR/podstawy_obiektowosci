f667581e445bd67c5757d521594696ce56d273dd
MaksymilianR <maks.rumianowski@onet.eu>
2022-10-26 19:30:54 +0200

    Stworzyłem królika

---
+++ src/Rabbit.java
@@ -0,0 +1,11 @@
+public class Rabbit {
+    int lengt;
+    String name;
+    boolean softness;
+
+    Rabbit(int lenght, String name, boolean softness){
+        this.lengt =lenght;
+        this.name =name;
+        this.softness =softness;
+    }
+}
